305b8b3e7e71acfece55564745b44ce271c4018f
Yusuf <yusuf_canakkale@hotmail.com>
2020-02-02 16:39:47 +0300

    Sail Animations, Top UI, Sea Manager added.

---
+++ Shipair/Assets/_Scripts/SeaManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeaManager : MonoBehaviour
+{
+    public float speed;
+    public GameObject[] seaObjects;
+    public float seaSpriteHeight;
+
+    private void Update()
+    {
+        GoForwardEffect();
+    }
+    public void GoForwardEffect()
+    {
+        for (int i = 0; i < seaObjects.Length; i++)
+        {
+            seaObjects[i].transform.localPosition += new Vector3(0,-(speed * Time.deltaTime),0);
+        }
+        if(seaObjects[0].transform.localPosition.y < -(seaSpriteHeight / 100f))
+        {
+            for (int i = 0; i < seaObjects.Length; i++)
+            {
+                seaObjects[i].transform.localPosition += new Vector3(0, +(seaSpriteHeight / 100f), 0);
+            }
+        }
+
+    }
+}
